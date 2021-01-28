2e32c52f58f15565d7e142931f87776e7d192d58
Tharindu Ketipearachchi <tharindu.k@aeturnum.com>
2021-01-28 14:04:39 +0530

    Font files added

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -14,7 +14,8 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(widget.title,
+            style: Theme.of(context).appBarTheme.textTheme.headline4),
       ),
       body: Center(
           child: Container(
