2641afb5922c04a14a0f385fe7a8d331255672e2
Tharindu Ketipearachchi <katramesh91@gmail.com>
2021-01-28 15:13:34 +0530

    Dev/app theme (#7)
    * package updated
    
    * themes data added
    
    * Font files added
    
    * package updated
    
    Co-authored-by: Tharindu Ketipearachchi <tharindu.k@aeturnum.com>

--- lib/commons/themes.dart
+++ lib/commons/themes.dart
@@ -1,147 +1,66 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:demo/extensions/color.dart';
 
+//Color theme generate https://material.io/resources/color/#!/?view.left=0&view.right=0&primary.color=03A9F4
+// https://maketintsandshades.com/#03a9f4
 ThemeData primaryTheme = ThemeData(
-    brightness: Brightness.light,
-    visualDensity: VisualDensity(vertical: 0.5, horizontal: 0.5),
-    primarySwatch: MaterialColor(
-      0xFFF5E0C3,
-      <int, Color>{
-        50: Color(0x1aF5E0C3),
-        100: Color(0xa1F5E0C3),
-        200: Color(0xaaF5E0C3),
-        300: Color(0xafF5E0C3),
-        400: Color(0xffF5E0C3),
-        500: Color(0xffEDD5B3),
-        600: Color(0xffDEC29B),
-        700: Color(0xffC9A87C),
-        800: Color(0xffB28E5E),
-        900: Color(0xff936F3E)
-      },
+  brightness: Brightness.light,
+  visualDensity: VisualDensity(vertical: 0.5, horizontal: 0.5),
+  primarySwatch: MaterialColor(
+    //https://api.flutter.dev/flutter/material/Colors-class.html
+    240116,
+    <int, Color>{
+      50: HexColor.fromHex("#b3e5fc"),
+      100: HexColor.fromHex("#9addfb"),
+      200: HexColor.fromHex("#81d4fa"),
+      300: HexColor.fromHex("#4fc3f7"),
+      400: HexColor.fromHex("#35baf6"),
+      500: HexColor.fromHex("#03a9f4"),
+      600: HexColor.fromHex("#0398dc"),
+      700: HexColor.fromHex("#0287c3"),
+      800: HexColor.fromHex("#0276ab"),
+      900: HexColor.fromHex("#007ac1")
+    },
+  ),
+  primaryColor: HexColor.fromHex("#03a9f4"),
+  fontFamily: 'Lato',
+  appBarTheme: AppBarTheme(
+      textTheme: TextTheme(
+    headline4: TextStyle(
+      fontFamily: 'Lato',
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
     ),
-    primaryColor: Color(0xffEDD5B3),
-    buttonTheme: ButtonThemeData(
-        //button themes
-        ),
-    toggleButtonsTheme: ToggleButtonsThemeData(
-        //toggle button theme
-        ),
-    buttonColor: Color(0xff936F3E),
-    secondaryHeaderColor: Colors.grey,
-    textSelectionColor: Color(0xffB5BFD3),
-    cursorColor: Color(0xff936F3E),
-    textSelectionHandleColor: Color(0xff936F3E),
-    backgroundColor: Color(0xff457BE0),
-    dialogBackgroundColor: Colors.white,
-    indicatorColor: Color(0xff457BE0),
-    hintColor: Colors.grey,
-    errorColor: Colors.red,
-    toggleableActiveColor: Color(0xff6D42CE),
-    textTheme: TextTheme(
-        //text themes that contrast with card and canvas
-        ),
-    primaryTextTheme: TextTheme(
-        //text theme that contrast with primary color
-        ),
-    accentTextTheme: TextTheme(
-        //text theme that contrast with accent Color
-        ),
-    inputDecorationTheme: InputDecorationTheme(
-        // default values for InputDecorator, TextField, and TextFormField
-        ),
-    iconTheme: IconThemeData(
-        //icon themes that contrast with card and canvas
-        ),
-    primaryIconTheme: IconThemeData(
-        //icon themes that contrast primary color
-        ),
-    accentIconTheme: IconThemeData(
-        //icon themes that contrast accent color
-        ),
-    sliderTheme: SliderThemeData(
-        // slider themes
-        ),
-    tabBarTheme: TabBarTheme(
-        // tab bat theme
-        ),
-    tooltipTheme: TooltipThemeData(
-        // tool tip theme
-        ),
-    cardTheme: CardTheme(
-        // card theme
-        ),
-    chipTheme: ChipThemeData(
-        backgroundColor: Color(0xff936F3E),
-        disabledColor: Color(0xaaF5E0C3),
-        shape: StadiumBorder(),
-        brightness: Brightness.light,
-        labelPadding: EdgeInsets.all(8),
-        labelStyle: TextStyle(),
-        padding: EdgeInsets.all(8),
-        secondaryLabelStyle: TextStyle(),
-        secondarySelectedColor: Colors.white38,
-        selectedColor: Colors.white
-        // chip theme
-        ),
-    platform: TargetPlatform.android,
-    materialTapTargetSize: MaterialTapTargetSize.padded,
-    applyElevationOverlayColor: true,
-    pageTransitionsTheme: PageTransitionsTheme(
-        //page transition theme
-        ),
-    appBarTheme: AppBarTheme(
-        //app bar theme
-        ),
-    bottomAppBarTheme: BottomAppBarTheme(
-        // bottom app bar theme
-        ),
-    colorScheme: ColorScheme(
-        primary: Color(0xffEDD5B3),
-        primaryVariant: Color(0x1aF5E0C3),
-        secondary: Color(0xffC9A87C),
-        secondaryVariant: Color(0xaaC9A87C),
-        brightness: Brightness.light,
-        background: Color(0xffB5BFD3),
-        error: Colors.red,
-        onBackground: Color(0xffB5BFD3),
-        onError: Colors.red,
-        onPrimary: Color(0xffEDD5B3),
-        onSecondary: Color(0xffC9A87C),
-        onSurface: Color(0xff457BE0),
-        surface: Color(0xff457BE0)),
-    snackBarTheme: SnackBarThemeData(
-        // snack bar theme
-        ),
-    dialogTheme: DialogTheme(
-        // dialog theme
-        ),
-    floatingActionButtonTheme: FloatingActionButtonThemeData(
-        // floating action button theme
-        ),
-    navigationRailTheme: NavigationRailThemeData(
-        // navigation rail theme
-        ),
-    typography: Typography.material2018(),
-    cupertinoOverrideTheme: CupertinoThemeData(
-        //cupertino theme
-        ),
-    bottomSheetTheme: BottomSheetThemeData(
-        //bottom sheet theme
-        ),
-    popupMenuTheme: PopupMenuThemeData(
-        //pop menu theme
-        ),
-    bannerTheme: MaterialBannerThemeData(
-        // material banner theme
-        ),
-    dividerTheme: DividerThemeData(
-        //divider, vertical divider theme
-        ),
-    buttonBarTheme: ButtonBarThemeData(
-        // button bar theme
-        ),
-    fontFamily: 'ROBOTO',
-    splashFactory: InkSplash.splashFactory);
+  )),
+  buttonTheme: ButtonThemeData(
+      //button themes
+      ),
+  toggleButtonsTheme: ToggleButtonsThemeData(
+      //toggle button theme
+      ),
+  buttonColor: Color(0xff936F3E),
+  secondaryHeaderColor: Colors.grey,
+  textSelectionColor: Color(0xffB5BFD3),
+  cursorColor: Color(0xff936F3E),
+  textSelectionHandleColor: Color(0xff936F3E),
+  backgroundColor: Color(0xff457BE0),
+  dialogBackgroundColor: Colors.white,
+  indicatorColor: Color(0xff457BE0),
+  hintColor: Colors.grey,
+  errorColor: Colors.red,
+  toggleableActiveColor: Color(0xff6D42CE),
+  textTheme: TextTheme(
+      //text themes that contrast with card and canvas
+      ),
+  primaryTextTheme: TextTheme(
+      //text theme that contrast with primary color
+      ),
+  platform: TargetPlatform.android,
+  materialTapTargetSize: MaterialTapTargetSize.padded,
+  applyElevationOverlayColor: true,
+);
 
 ThemeData darkTheme = ThemeData(
   brightness: Brightness.dark,
