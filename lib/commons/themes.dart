3b8b4ca92c88c281bddb00eceadce5c9990564ba
tharinduKetipe <katramesh91@gmail.com>
2022-01-04 17:23:40 +0530

    style issues fixed

--- lib/commons/themes.dart
+++ lib/commons/themes.dart
@@ -1,15 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:demo/extensions/color.dart';
 
 //Color theme generate https://material.io/resources/color/#!/?view.left=0&view.right=0&primary.color=03A9F4
 // https://maketintsandshades.com/#03a9f4
-
 ThemeData primaryTheme = ThemeData(
   brightness: Brightness.light,
   visualDensity: VisualDensity(vertical: 0.5, horizontal: 0.5),
   primarySwatch: MaterialColor(
     //https://api.flutter.dev/flutter/material/Colors-class.html
-    240116, //primary color in integer format #03a9f4
+    240116,
     <int, Color>{
       50: HexColor.fromHex("#b3e5fc"),
       100: HexColor.fromHex("#9addfb"),
@@ -35,9 +35,7 @@ ThemeData primaryTheme = ThemeData(
           color: Colors.white,
         ),
       )),
-  buttonTheme: ButtonThemeData(
-      //button themes
-      ),
+  buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.primary),
   toggleButtonsTheme: ToggleButtonsThemeData(
       //toggle button theme
       ),
@@ -49,15 +47,42 @@ ThemeData primaryTheme = ThemeData(
   backgroundColor: Color(0xff457BE0),
   dialogBackgroundColor: Colors.white,
   indicatorColor: Color(0xff457BE0),
-  hintColor: Colors.grey,
+  hintColor: HexColor.fromHex("#bcbec0"),
   errorColor: Colors.red,
   toggleableActiveColor: Color(0xff6D42CE),
+  inputDecorationTheme: InputDecorationTheme(
+    labelStyle: TextStyle(
+      fontFamily: 'Lato',
+      fontSize: 14.0,
+      fontWeight: FontWeight.normal,
+      color: HexColor.fromHex("#414042"),
+    ),
+  ),
   textTheme: TextTheme(
-      //text themes that contrast with card and canvas
-      ),
+    headline4: TextStyle(
+      fontFamily: 'Lato',
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    ),
+    subtitle1: TextStyle(
+      fontFamily: 'Lato',
+      fontSize: 18,
+      fontWeight: FontWeight.normal,
+    ),
+    bodyText1: TextStyle(
+      fontFamily: 'Lato',
+      fontSize: 18,
+      fontWeight: FontWeight.normal,
+    ),
+  ),
   primaryTextTheme: TextTheme(
-      //text theme that contrast with primary color
-      ),
+    headline5: TextStyle(
+      fontFamily: 'Lato',
+      fontSize: 16,
+      fontWeight: FontWeight.normal,
+    ),
+  ),
   platform: TargetPlatform.android,
   materialTapTargetSize: MaterialTapTargetSize.padded,
   applyElevationOverlayColor: true,
